f4eb0bde61e6ea1db99078b9c1b8771cc254c612
Yannick GICQUEL <ygicquel@gmail.com>
2014-11-02 10:54:08 +0100

    docbook: empty doc template update
    Signed-off-by: Yannick GICQUEL <ygicquel@gmail.com>

--- libqhe/docbookdoc.py
+++ libqhe/docbookdoc.py
@@ -5,23 +5,27 @@ from lxml import etree
 from gi.repository import GtkSource
 from .htmldoc import *
 
-TEXT_DEFAULT="""<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE chapter PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN"
-"http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd">
-<chapter>
-  <title>Chapter Title Here</title>
-
-  <para>If you have any opening text before the first section heading, put it
-  here. (Do not use a <literal>&lt;sect1&gt;</literal> with an empty
-  <literal>&lt;title&gt;</literal>.) If you want to start your chapter with a
-  section heading, just delete this <literal>&lt;para&gt;</literal>.</para>
-
-  <sect1>
-    <title>Section Title Here</title>
-
-    <para>Opening para...</para>
-  </sect1>
-</chapter>
+TEXT_DEFAULT="""<!DOCTYPE article PUBLIC "-//OASIS//DTD DocBook V3.1//EN">
+<article>
+<artheader>
+<title>Docbook article introduction</title>
+<author><honorific>M</honorific><firstname>Yannick</firstname>
+<surname>Gicquel</surname></author>
+</artheader>
+<para> ... </para>
+<section>
+<title>Introduction</title>
+<section>
+<title>Welcome to the wonderful world of DocBook.</title>
+<para>This tools is quite a good thing for <emphasis>real</emphasis> document
+typesetting. It is possible to do cool stuff with this tool.
+</section>
+</para>
+<para>Welcome to the wonderful world of DocBook.</para>
+<para> ... </para>
+</section>
+<bibliography> ... </bibliography>
+</article>
 """
 
 class docbookdoc(htmldoc):
